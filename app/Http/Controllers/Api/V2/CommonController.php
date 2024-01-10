fb64c036e81d7743fb7e9ca723f7367a11848f79
Jishap-Tomsher <130639775+Jishap-Tomsher@users.noreply.github.com>
2024-01-10 12:12:58 +0400

    app home banners

--- app/Http/Controllers/Api/V2/CommonController.php
+++ app/Http/Controllers/Api/V2/CommonController.php
@@ -299,7 +299,7 @@ class CommonController extends Controller
 
     public function homeAdBanners()
     {
-        $all_banners = Banner::with(['mainImage'])->where('status', true)->get();
+        $all_banners = Banner::with(['mobileImage'])->where('status', true)->get();
 
         $banner_id = BusinessSetting::whereIn('type', [
             'app_banner_1',
@@ -322,7 +322,7 @@ class CommonController extends Controller
                             // 'image1' => $c_banner,
                             'link_type' => $c_banner->link_type ?? '',
                             'link_id' => $c_banner->link_type == 'external' ? $c_banner->link : $c_banner->link_ref_id,
-                            'image' => storage_asset($c_banner->mainImage->file_name)
+                            'image' => storage_asset($c_banner->mobileImage->file_name)
                         );
                     }else{
                         $banners[$banner->type][] = null;
