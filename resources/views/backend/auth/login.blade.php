ba7535c3e79a284ce4a5d3ac09c87ea420d8dec3
Jishap-Tomsher <130639775+Jishap-Tomsher@users.noreply.github.com>
2024-02-23 12:18:58 +0400

    back changes

--- resources/views/backend/auth/login.blade.php
+++ resources/views/backend/auth/login.blade.php
@@ -8,8 +8,8 @@
                     <div class="card text-left">
                         <div class="card-body">
                             <div class="mb-5 text-center">
-                                <img src="{{ static_asset('assets/img/tlogo.png') }}" class="mw-100 mb-4" height="70">
-                                {{-- <h1 class="h3 text-primary mb-0">Welcome to Medon</h1> --}}
+                                <img src="{{ static_asset('assets/img/logo.png') }}" class="mw-100 mb-4" height="70">
+                                <h1 class="h3 text-primary mb-0">Welcome to Medon</h1>
                                 <p>Login to your account.</p>
                             </div>
                             <form class="pad-hor" method="POST" role="form" action="{{ route('admin.login') }}">
