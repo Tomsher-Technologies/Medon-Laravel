2eb1e9b3475501209d7f3725c892932c0656253b
Jishap-Tomsher <130639775+Jishap-Tomsher@users.noreply.github.com>
2024-05-11 13:33:25 +0400

    no category breadcrumb issue

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -46,5 +46,7 @@ Breadcrumbs::for('product', function (BreadcrumbTrail $trail, Product $product) 
 
 // Product
 Breadcrumbs::for('product_admin', function (BreadcrumbTrail $trail, Product $product) {
-    $trail->parent('category_no_home', $product->category);
+    if($product->category){
+        $trail->parent('category_no_home', $product->category);
+    }
 });
